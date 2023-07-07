95aa643676a3feb154b96f36591ce80f211e943c
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2023-07-07 10:46:31 -0300

    img whastapp

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,18 +6,16 @@ import 'react-quill/dist/quill.snow.css';
 import 'quill-emoji/dist/quill-emoji.css';
 import Swal from 'sweetalert2';
 
-
-
-
 function Input() {
 	const editorRef = useRef(null);
-	let textToFormat = '';
-
-  const [formattedText,setFormattedText] = useState('')
+	// let textToFormat = '';
+  const [empty,setTextEmpty] = useState(true)
+	const [textToFormat, setTextToFormat] = useState('');
+	const [formattedText, setFormattedText] = useState('');
 	const modules = {
 		toolbar: [
 			[{ size: [] }],
-			['bold', 'italic','strike'],
+			['bold', 'italic', 'strike'],
 			[
 				{ list: 'ordered' },
 				{ list: 'bullet' },
@@ -47,49 +45,59 @@ function Input() {
 	];
 
 	const handleFormat = (e, delta, source, editor) => {
-		const text = editor.getHTML();
-		textToFormat = text;
-		console.log('textToFormat', textToFormat);
+    let textGetted = editor.getText()
+    if(!textGetted){
+      setTextEmpty(true)
+    }else{
+      setTextEmpty(false)
+    }
+		setTextToFormat(editor.getHTML());
+
+     if (!empty && formattedText) {
+				setFormattedText('');
+			}
 	};
+  // console.log(textGetted)
+	console.log('textToFormat', textToFormat);
 
-const formatTextToWhatsApp = () => {
-  
-  if (!textToFormat) {
-		Swal.fire({
-			icon: 'error',
-			title: 'No hay texto',
-			text: 'Por favor, ingresa un texto antes de formatear y copiar al portapapeles.',
-			timer: 3000, 
-			showConfirmButton: false,
-		});
-		return;
-	}
+	const formatTextToWhatsApp = () => {
+		if (!textToFormat) {
+			Swal.fire({
+				icon: 'error',
+				title: 'No hay texto',
+				text: 'Por favor, ingresa un texto antes de formatear y copiar al portapapeles.',
+				timer: 3000,
+				showConfirmButton: false,
+			});
+			return;
+		}
 
-	let textFormatted = textToFormat.replace(/<\/?p>/gi, '');
-	textFormatted = textFormatted.replace(/<strong>(.*?)<\/strong>/gi, '*$1*');
-	textFormatted = textFormatted.replace(/<em>(.*?)<\/em>/gi, '_$1_');
-	textFormatted = textFormatted.replace(/<s>(.*?)<\/s>/gi, '~$1~');
-	textFormatted = textFormatted.replace(/<ul>(.*?)<\/ul>/gi, '\n$1');
-	textFormatted = textFormatted.replace(/<ol>(.*?)<\/ol>/gi, '\n$1');
-	textFormatted = textFormatted.replace(/<li>(.*?)<\/li>/gi, '• $1\n');
-	textFormatted = textFormatted.replace(/<\/?span[^>]*>/gi, '');
+		let textFormatted = textToFormat.replace(/<\/?p>/gi, '');
+		textToFormat.replace(/<\/?br>/gi, '');
+		textFormatted = textFormatted.replace(/<strong>(.*?)<\/strong>/gi, '*$1*');
+		textFormatted = textFormatted.replace(/<em>(.*?)<\/em>/gi, '_$1_');
+		textFormatted = textFormatted.replace(/<s>(.*?)<\/s>/gi, '~$1~');
+		textFormatted = textFormatted.replace(/<ul>(.*?)<\/ul>/gi, '\n$1');
+		textFormatted = textFormatted.replace(/<ol>(.*?)<\/ol>/gi, '\n$1');
+		textFormatted = textFormatted.replace(/<li>(.*?)<\/li>/gi, '• $1\n');
+		textFormatted = textFormatted.replace(/<\/?span[^>]*>/gi, '');
 
+		setFormattedText(textFormatted);
+		console.log('formattedText', formattedText);
 
-  setFormattedText(textFormatted);
-	console.log('formattedText', formattedText);
-
-   navigator.clipboard.writeText(textFormatted).then(() => {
+		navigator.clipboard.writeText(textFormatted).then(() => {
 			console.log('Texto copiado al portapapeles');
 		});
 
-    Swal.fire({
+		Swal.fire({
 			icon: 'success',
 			title: '¡Texto Formateado y copiado!',
 			footer: '<a href="">Ahora solo queda pegarlo en el chat 😁</a>',
 			timer: 1000,
 			showConfirmButton: false,
 		});
-};
+   
+	};
 
 	return (
 		<div>
@@ -105,8 +113,21 @@ const formatTextToWhatsApp = () => {
 						}}
 					/>
 				</div>
-				<div className=' h-[400px] w-1/3 mt-3'>
-					<div className=' h-full'>{formattedText}</div>
+				<div className=' w-1/3 mt-3'>
+					{!empty && formattedText ? (
+						<div className=''>{formattedText}</div>
+					) : (
+						<div className='flex flex-col items-center justify-center h-full'>
+							<p className='font-bold italic text-center'>
+								Aquí aparecerá el texto formateado
+							</p>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
+								alt='whatsapp-logo'
+								className='w-32 h-32 mt-4'
+							/>
+						</div>
+					)}
 				</div>
 			</div>
 			<div className='text-center'>
